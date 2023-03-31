ae7fd5ee30c76fecace8932e2c3da723bcb9b01f
Jake Velasco <59675743+Jayke770@users.noreply.github.com>
2023-03-31 14:12:29 +0800

    🔒 chore(ecosystem.config.js): update OpenAI API key

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         env_production: {
             NODE_ENV: "production",
             BOT_TOKEN: "6182381541:AAGK_SWovrMFdodR8ywGY9-ElUGVnC-MCOU",
-            OPENAI_API_KEY: "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"
+            OPENAI_API_KEY: "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"
         }
     }]
 }
