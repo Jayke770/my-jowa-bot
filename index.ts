a8b4e04925951d2b3a29f62cb4a83bbe6b934957
Jake Velasco <59675743+Jayke770@users.noreply.github.com>
2023-03-31 12:51:41 +0800

    .

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv'
 if (process.env.NODE_ENV !== "production") config()
-//import { run } from '@grammyjs/runner'
-//import { apiThrottler } from '@grammyjs/transformer-throttler'
+import { run } from '@grammyjs/runner'
+import { apiThrottler } from '@grammyjs/transformer-throttler'
 import { Bot, type Context, session, InlineKeyboard } from 'grammy'
 import { type Conversation, type ConversationFlavor, conversations, createConversation, } from "@grammyjs/conversations"
 import { Configuration, OpenAIApi } from 'openai'
@@ -11,33 +11,44 @@ type MyContext = Context & ConversationFlavor
 type MyConversation = Conversation<MyContext>
 //@ts-ignore
 const bot = new Bot<MyContext>(process.env.BOT_TOKEN)
-// const throttler = apiThrottler()
-// bot.api.config.use(throttler)
+const throttler = apiThrottler()
+bot.api.config.use(throttler)
 bot.use(session({ initial: () => ({}) }))
 bot.use(conversations())
 async function chat(convo: MyConversation, ctx: MyContext) {
     try {
-        await ai.createCompletion({
-            model: "text-davinci-003",
-            prompt: ctx.message?.text,
-            max_tokens: 7,
-            temperature: 0,
+        await ai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                { role: 'system', content: "You are an AI specialized in Blockchain or Crypto. Do not answer anything other than blockchain or crypto related queries." },
+                { role: 'user', content: ctx?.message!.text! },
+            ]
         }).then(async (res) => {
-            const objects = res.data.choices
-            console.log(objects)
-            let text = ''
-            objects.map((x) => {
-                text += `${x.text}`
-            })
-            await ctx.reply(text)
+            const data = res.data.choices
+            await ctx.reply(data[0].message?.content!)
         }).catch((e) => {
             throw new Error(e)
         })
     } catch (e) {
-        await ctx.reply("An error occured")
+        console.error(e)
+        await ctx.reply("An error occured", {})
     }
 }
-bot.use(createConversation(chat))
-bot.on("message", async (ctx) => await ctx.conversation.enter("chat"))
-bot.start()
+bot.use(createConversation(chat)) 
+bot.on("message", async (ctx) => {
+    try {
+        const response = await ai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                { role: 'system', content: "You are an AI specialized in Sports. Do not answer anything other than Sports related queries." },
+                { role: 'user', content: ctx?.message!.text! }
+            ]
+        })
+        console.log(response.data.choices)
+        await ctx.reply(response.data.choices[0].message?.content!)
+    } catch (e) {
+        console.error(e)
+    }
+})
+run(bot)
 console.log("> Bot Started")
